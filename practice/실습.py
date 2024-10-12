3d7a4667d8c93be6b11c631df3060f293965f695
User <ndh080929@gmail.com>
2024-10-12 10:45:26 +0900

    boolean(True/False) 예제 숫자연산 예제

---
+++ practice/실습.py
@@ -0,0 +1,17 @@
+# 사용자에게 수학 점수를 입력받아
+# 상/중/하 반으로 분류
+# 90점 이상
+# 70점 이상
+# 나머지
+
+size = int(input())
+print(size)
+
+if size > 100:
+    print("잘못된 점수입니다. 제대로 입력하세요")
+elif size >= 90:
+    print("상")
+elif size >= 70:
+    print("중")
+else:
+    print("하")
