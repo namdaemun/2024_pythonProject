35f0bf2f6fdd580d7cd91b96b5fa1edcce811528
User <ndh080929@gmail.com>
2024-11-09 12:06:08 +0900

    len

--- practice/06-1.while-example-map.py
+++ practice/06-1.while-example-map.py
@@ -1,13 +1,46 @@
-a = str(input())
-x = 0
-y = 0
+# a = str(input())
+# x = 0
+# y = 0
+#
+#
+# if a == "UP" :
+#     y = y+1
+# elif a == "DOWN" :
+#     y = y-1
+# elif a == "LEFT" :
+#     x = x-1
+# elif a == "RIGHT":
+#     x = x+1
 
 
-if a == "UP" :
-    y = y+1
-elif a == "DOWN" :
-    y = y-1
-elif a == "LEFT" :
-    x = x-1
-elif a == "RIGHT":
-    x = x+1
+user_input = ""
+X = 0
+Y = 0
+
+while user_input != "EXIT":
+    while user_input != "EXIT" and user_input != "UP" and user_input != "DOWN" and user_input != "LEFT" and user_input != "RIGHT":
+        user_input = input("움직일 방향을 입력하세요 (종료는 EXIT) :: ")
+    print(user_input)
+
+    if user_input == "UP":
+        if Y < 100:
+            Y = Y + 1
+        else:
+            print("범위를 벗어날 수 없습니다.")
+    elif user_input == "DOWN":
+        if Y > 0:
+            Y = Y - 1
+        else:
+            print("범위를 벗어날 수 없습니다.")
+    elif user_input == "LEFT":
+        if X > 0:
+            X = X - 1
+        else:
+            print("범위를 벗어날 수 없습니다.")
+    elif user_input == "RIGHT":
+        if X < 100:
+            X = X + 1
+        else:
+            print("범위를 벗어날 수 없습니다.")
+    user_input = ""
+print("프로그램을 종료합니다.")
